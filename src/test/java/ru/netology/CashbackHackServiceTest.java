60b65e9202a496abfc37abbdaf44917caf37815a
SemNik <semnikg@gmail.com>
2024-01-31 22:51:00 +0300

    TestNG

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,35 +1,31 @@
 package ru.netology;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 public class CashbackHackServiceTest {
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void shouldAddAmountIfLess1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int expected = 100;
         int actual = service.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.Assert.assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void shouldAddAmountIfMore1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1200;
         int expected = 800;
         int actual = service.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.Assert.assertEquals(actual, expected);
     }
 
-    @org.junit.Test
+    @org.testng.annotations.Test
     public void shouldNotAddAmountIf1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int expected = 0;
         int actual = service.remain(amount);
-        org.junit.Assert.assertEquals(expected, actual);
+        org.testng.Assert.assertEquals(actual, expected);
     }
 }
