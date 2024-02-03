55aba4f5649bfaa8a07e5378fe64edaafd82a78d
SemNik <142649558+SemNik88@users.noreply.github.com>
2024-02-03 19:50:19 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,31 +1,37 @@
 package ru.netology;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
-    public void shouldAddAmountIfLess1000() {
+    @Test
+    public void remainIfAmountEqualBoundary() {
         CashbackHackService service = new CashbackHackService();
-        int amount = 900;
-        int expected = 100;
-        int actual = service.remain(amount);
-        org.testng.Assert.assertEquals(actual, expected);
-    }
 
-    @org.testng.annotations.Test
-    public void shouldAddAmountIfMore1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1200;
-        int expected = 800;
-        int actual = service.remain(amount);
-        org.testng.Assert.assertEquals(actual, expected);
-    }
-
-    @org.testng.annotations.Test
-    public void shouldNotAddAmountIf1000() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
+        int actual = service.remain(1000);
         int expected = 0;
-        int actual = service.remain(amount);
-        org.testng.Assert.assertEquals(actual, expected);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainIfAmountLessBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void remainIfAmountMoreBoundary() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 400;
+        int actual = service.remain(1600);
+        assertEquals(expected, actual);
     }
 }
